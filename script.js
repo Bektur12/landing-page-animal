5765b92557970d834376803fcee6bdffa466f645
Bektur12 <kanybekovbektur22@gmail.com>
2023-01-08 22:02:34 +0600

    finished three block

--- script.js
+++ script.js
@@ -11,20 +11,29 @@ $(document).ready(function () {
       '<button type="button" class="next"><img src="./image/right-arrow.svg"/></button>',
   });
 });
+
+$(".slider_animal_inner").slick({
+  infinite: true,
+  speed: 1500,
+  slidesToShow: 2,
+  adaptiveHeight: true,
+  centerMode: true,
+  variableWidth: true,
+  autoplaySpeed: 2000,
+  slidesToScroll: 3,
+  arrows: false,
+});
+
 $(document).ready(function () {
   $(".slider_animal_inner").slick({
     infinite: true,
-    speed: 500,
-    fade: true,
-
-    cssEase: "linear",
+    speed: 1500,
+    slidesToShow: 2,
+    adaptiveHeight: true,
     centerMode: true,
-    centerPadding: "60px",
-    slidesToShow: 3,
-    prevArrow:
-      '<button type="button" class="prev"><img src="./image/riight.svg"/></button>',
-
-    nextArrow:
-      '<button type="button" class="next"><img src="./image/right-arrow.svg"/></button>',
+    variableWidth: true,
+    autoplaySpeed: 2000,
+    slidesToScroll: 3,
+    arrows: false,
   });
 });
