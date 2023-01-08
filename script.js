7409f3f0f42201437f84bbf203ed04f8c82cc34b
Bektur12 <kanybekovbektur22@gmail.com>
2023-01-08 20:45:01 +0600

    I start the second slider

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ $(document).ready(function () {
       '<button type="button" class="next"><img src="./image/right-arrow.svg"/></button>',
   });
 });
+$(document).ready(function () {
+  $(".slider_animal_inner").slick({
+    infinite: true,
+    speed: 500,
+    fade: true,
+
+    cssEase: "linear",
+    centerMode: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    prevArrow:
+      '<button type="button" class="prev"><img src="./image/riight.svg"/></button>',
+
+    nextArrow:
+      '<button type="button" class="next"><img src="./image/right-arrow.svg"/></button>',
+  });
+});
